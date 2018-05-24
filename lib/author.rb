c0082de514ec240dee2e56aae8ea4ccb4fd8b96a
ClaireBvs <clairembeauvais@gmail.com>
2018-05-24 10:52:03 -0600

    implements method add_book

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,22 @@
-class Author
+require 'Date'
 
+class Author
+  attr_reader :first_name,
+              :last_name,
+              :books
+
+  def initialize(author)
+    @first_name = author[:first_name]
+    @last_name  = author[:last_name]
+    @books = []
+  end
+
+  def add_book(title, publication_date)
+    publication_year = publication_date.split.last
+    book = Book.new({author_first_name: first_name, author_last_name: last_name, title: title, publication_date: publication_year})
+    @books << book
+    return book
+  end
+
+  Date.strptime("12/22/2011", "%m/%d/%Y")
 end
--- test/author_test.rb
+++ test/author_test.rb
@@ -1,6 +1,37 @@
 require './test/test_helper'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/author'
+require './lib/book'
 
 class AuthorTest < Minitest::Test
 
+  def test_it_exists
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
 
+    assert_instance_of Author, charlotte_bronte
+  end
+
+  def test_it_has_no_book_at_first
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    assert_equal [], charlotte_bronte.books
+  end
+
+  def test_author_add_book_to_collection
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    book = Book.new({author_first_name: "Emilie", author_last_name: "Bronte", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    assert_instance_of Book, charlotte_bronte.add_book("Jane Eyre", "October 16, 1847")
+    assert_equal "Charlotte", charlotte_bronte.first_name
+  end
+
+  def test_can_add_books_to_array
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    book = Book.new({title: "Jane Eyre", publication_date: "10/16/1847"})
+    charlotte_bronte.add_book("Jane Eyre", "October 16, 1847")
+
+    assert_equal [book], charlotte_bronte.books
+    # assert_equal "Charlotte", charlotte_bronte.first_name
+  end
 end
